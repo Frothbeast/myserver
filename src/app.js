0f9e30ac057ea24b49dc30f8c75d76dc85fea6af
Frothbeast <frothbeast@gmail.com>
2025-04-09 12:50:23 -0400

    I am messed up and have no idea what I am doing
    Signed-off-by: Frothbeast <frothbeast@gmail.com>

---
+++ src/app.js
@@ -0,0 +1,12 @@
+
+function Greeter() {
+    return <button>See Pictures</button>
+}
+export default function App() {
+    return (
+        <div className="App">
+            <h1>FrothServer</h1>
+            <Greeter/>
+        </div>
+    )
+}
