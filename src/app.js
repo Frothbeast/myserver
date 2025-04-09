968496851e2e05dd7a5a88f5893ab45d4a2c3ca1
Frothbeast <frothbeast@gmail.com>
2025-04-09 13:59:09 -0400

    nope

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
-
-function Greeter() {
-    return <button>See Pictures</button>
-}
 export default function App() {
     return (
         <div className="App">
             <h1>FrothServer</h1>
-            <Greeter/>
+
         </div>
     )
 }
