77fd96a269be2641272f5c7745a1b59937dbff5e
Frothbeast <frothbeast@gmail.com>
2025-04-09 13:13:10 -0400

    sameses
    Signed-off-by: Frothbeast <frothbeast@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App";
+import App from "./app";
 
 const rootElement =  document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
     <StrictMode>
+        <h1>wtf</h1>
         <App />
     </StrictMode>
 );
