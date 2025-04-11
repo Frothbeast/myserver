186fb45df99fda154879236c4b372a5bb1b626b1
Frothbeast <frothbeast@gmail.com>
2025-04-11 10:34:25 -0400

    consoliddating files still
    Signed-off-by: Frothbeast <frothbeast@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,11 @@
+ function App() {
+    return (
+        <div className="App">
+            <h1>FrothServer</h1>
+            <h2>Success!</h2>
+            <button>Add Photo</button>
+        </div>
+    )
+}
+
+export default App;
