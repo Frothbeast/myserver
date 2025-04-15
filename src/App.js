7ddc6addeba24fb4e55ff39247f589ab3af5f36e
Frothbeast <frothbeast@gmail.com>
2025-04-15 16:28:09 -0400

    first functional btn click function.  just learning.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
- function App() {
-    return (
-        <div className="App">
-            <h1>FrothServer</h1>
-            <h2>Sucks ass!</h2>
-            <button>Add Photo</button>
-        </div>
-    )
-}
+import { useState, useEffect } from 'react';
+    function App() {
+        const [counter, setCounter] = useState(0);
+        const [clickCounter, setClickCounter] = useState(0)
+        useEffect(() => {
+        const interval = setInterval(() => {
+            setCounter((prevCounter) => prevCounter + 1);
+        }, 1000);
+        
+        return () => clearInterval(interval);
+        }, []);
+
+        const btnClick = () => {
+            setCounter(0);
+            setClickCounter(clickCounter + 1);
+        }
+        
+        return (
+            <div className="App">
+                <h1>FrothServer</h1>
+                <h2>Time Online {counter}</h2>
+                <h2>Number of clicks {clickCounter}</h2>
+                <button onClick={btnClick} >reset timer</button>
+            </div>
+        )
+    }
 
 export default App;
