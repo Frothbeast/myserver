b5b2dc26eec0e9f17c0636f8c48120b035a1b325
Frothbeast <frothbeast@gmail.com>
2025-04-21 09:54:20 -0400

    added rounding
    Signed-off-by: Frothbeast <frothbeast@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react';
     function App() {
+        const [timeUp, setTimeUp] = useState(0);
         const [counter, setCounter] = useState(0);
+        const [totalTime, setTotalTime] = useState(0);
         const [clickCounter, setClickCounter] = useState(0)
+        const startTime = Date.now();
         useEffect(() => {
         const interval = setInterval(() => {
             setCounter((prevCounter) => prevCounter + 1);
+            setTotalTime(Math.round((Date.now() - startTime)/1000))
         }, 1000);
         
         return () => clearInterval(interval);
@@ -13,13 +17,15 @@ import { useState, useEffect } from 'react';
         const btnClick = () => {
             setCounter(0);
             setClickCounter(clickCounter + 1);
+            
         }
         
         return (
             <div className="App">
-                <h1>FrothServer</h1>
-                <h2>Time Online {counter}</h2>
-                <h2>Number of clicks {clickCounter}</h2>
+                <h1>FrothServer </h1>
+                <h1>Server webpage time since restart {totalTime}</h1>
+                <h2>Session Time Since last reset {counter}</h2>
+                <h2>Number of clicks of reset {clickCounter}</h2>
                 <button onClick={btnClick} >reset timer</button>
             </div>
         )
